8d3f5cb6a7cf38100fa5f4e8fc241d21b5a93e00
loveCanopy <1039431583@qq.com>
2016-07-05 15:37:25 +0800

    动态规划求最大和子数组

---
+++ maxSubArray.java
@@ -0,0 +1,33 @@
+package D13;
+
+
+
+//数组，找出连续的一段，其和最大
+//使用动态规划 sum[i]=max(num[i],sum[i-1]+num[i])
+
+public class maxSubArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] a={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxArray(a));
+        
+	}
+
+	
+	public static int maxArray(int[] a){
+		
+		int len=a.length;
+		int min;
+		int res=0;
+		int[] sum=new int[len];
+		for(int i=1;i<len-1;i++){
+			sum[i]=Math.max(sum[i-1]+a[i],a[i]);
+			res=Math.max(sum[i], res);
+		}
+	return res;	
+	}
+	
+	
+	
+}
